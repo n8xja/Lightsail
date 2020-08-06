cfeb34636aa84580bd512e91b32b479ada8b0a77
n8xja <n8xja@users.noreply.github.com>
2020-08-06 17:02:41 -0400

    Update increase_PHP_UPload_Limit.sh
    fixed error. added diff at end.

--- increase_PHP_UPload_Limit.sh
+++ increase_PHP_UPload_Limit.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
+TARGET="/opt/bitnami/php/etc/php.ini"
 
-TF="/opt/bitnami/php/etc/php.ini"
+TMPFILE="$HOME/php.ini.tmp"
 
-TFT="$TF.tmp"
+echo "$TMPFILE"
+echo "Modifying $TARGET ... hold on..."
 
-echo -n "Modifying $TF ... hold on..."
 
 #create backup of orig php.ini file
-sudo cp --preserve $TF $TF.save
+sudo cp -v --preserve $TARGET $TARGET.save
+
+
+
 
 # use SED magic to change the default limit from 40M to 128M
 # write result to temp file
-sudo cat $TF | sed -e 's/post_max_size = 40M/post_max_size = 128M/g' | sed -e 's/upload_max_filesize = 40M/upload_max_filesize = 128M/g' > $TFT
+
+sudo cat $TARGET | sed -e 's/post_max_size = 40M/post_max_size = 128M/g' | sed -e 's/upload_max_filesize = 40M/upload_max_filesize = 128MF/g' > $TMPFILE
 
 #copy TMP file to production file, leaving privs alone
-sudo cat $TFT > $TF 
+
+sudo cat $TMPFILE > $TARGET
+
+# remove temp file:
+rm -f $TMPFILE
+
 
 echo "Done.... Miss me? "
 
 echo "restarting......"
 sudo /opt/bitnami/ctlscript.sh restart
+
+
+diff $TARGET $TARGET.save
